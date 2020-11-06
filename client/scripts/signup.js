43bee88771a19305fb744cd5bb7988a80298640f
Louis <louismartin@umass.edu>
2020-11-05 20:34:28 -0500

    Implemented user signup. Alerts the user that a new user was created with the given email and password.

---
+++ client/scripts/signup.js
@@ -0,0 +1,39 @@
+'use strict'
+
+let form = document.getElementById("signupForm");
+
+
+document.getElementById("signupButton").addEventListener("click", async function(event){
+    let email = form.elements.email.value;
+    let password = form.elements.password.value;
+    let confirmPassword = form.elements.confirmPassword.value;
+    if(password === confirmPassword){
+        let res = await fetch(`/signup?email=${email}&password=${password}`);
+        if (!res.ok) {
+            console.log(res.error);
+            return;
+        }
+        else{
+            let data = await res.json();
+            console.log(data.email);
+            if(data){
+                alert("user created with email: " + data.email + "\npassword: " + data.password);
+            }
+            else{
+                console.log(data);
+            }
+            
+        }
+    }
+    else{
+        alert("Your Password does not match!");
+    }
+    
+    event.preventDefault();
+});
+
+
+form.addEventListener("submit", function(event) {
+    console.log("Saving value", form.elements.value.value);
+    event.preventDefault();
+  });
