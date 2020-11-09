bf332a89111d6cfe9acb31293a4e45509cc5b5fd
Bkhamilton <43786988+Bkhamilton@users.noreply.github.com>
2020-11-09 18:24:46 -0500

    Not Done Yet

--- client/scripts/login.js
+++ client/scripts/login.js
@@ -2,3 +2,30 @@
 
 let loginForm = document.getElementById("loginForm");
 
+
+document.getElementById("loginButton").addEventListener("click", async function(event) {
+    let email = loginForm.elements.loginEmail.value;
+    let password = loginForm.elements.loginPassword.value;
+    
+    let res = await fetch('/user/read?email=${email}&password=${password}');
+    if (!res.ok) {
+        console.log(res.error)
+        return;
+    } else {
+        let data = await res.json();
+        console.log(data.email);
+        if (data) {
+            alert("Successfully logged in");
+        } else {
+            console.log(data);
+        }
+        
+    }
+  
+    event.preventDefault();
+}
+                                                        
+loginForm.addEventListener("submit", function(event) {
+    
+  
+} 
