64c5d82dd4ab4eb4a25646292d993f8e6302837b
Bkhamilton <43786988+Bkhamilton@users.noreply.github.com>
2020-11-09 21:14:40 -0500

    changed fetch line

--- client/scripts/login.js
+++ client/scripts/login.js
@@ -7,7 +7,7 @@ document.getElementById("loginButton").addEventListener("click", async function(
     let email = loginForm.elements.loginEmail.value;
     let password = loginForm.elements.loginPassword.value;
     
-    let res = await fetch('/users');
+    let res = await fetch('/user/login');
     if (!res.ok) {
         console.log(res.error)
         return;
