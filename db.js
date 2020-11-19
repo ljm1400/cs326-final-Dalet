ba869cd9eef9b3a5b1552e19750e809f77d07eea
Louis <louismartin@umass.edu>
2020-11-19 17:24:51 -0500

    Added outline of db functions and required db.js in server.

--- db.js
+++ db.js
@@ -1,15 +1,19 @@
 'use strict'
 let secrets;
 let password;
+let username;
 if (!process.env.PASSWORD) {
-secrets = require('secrets.json');
+secrets = require('./secrets.json');
 username = secrets.USERNAME;
 password = secrets.PASSWORD;
 } else {
-    username = process.env.USERNAME
+  username = process.env.USERNAME
 	password = process.env.PASSWORD;
 }
+console.log(username);
+console.log(password);
 const uri = `mongodb+srv://${username}:${password}@cluster0.oig9w.mongodb.net/Cluster0?retryWrites=true&w=majority`;
+console.log(uri);
 const MongoClient = require('mongodb').MongoClient;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -20,3 +24,55 @@ const cleanup = (event) => { // SIGINT is sent for example when you Ctrl+C a run
   
 process.on('SIGINT', cleanup);
 process.on('SIGTERM', cleanup);
+client.connect(err => {
+  if (err) {
+      console.error(err);
+  }else{
+    console.log("DB connected")
+  }
+  
+});
+//Adds a new user to the database
+function addUser(user){
+
+}
+
+//gets a user with a given username from the database
+function getUser(username){
+
+}
+
+//updates a given user's information from newUserInfo
+function updateUser(username, newUserInfo){
+
+}
+
+//gets all of the users from the database, modified to not include sensitive information
+function getUsers(){
+
+}
+
+//adds a newly created post to the database
+function createPost(post){
+
+}
+
+//gets all of the posts from the database
+function getPosts(){
+
+}
+
+//gets all of the posts from the database of type climbing
+function getClimbingPosts(){
+
+}
+
+//gets all of the posts from the database of type hiking
+function getHikingPosts(){
+
+}
+
+//update a given 
+function updatePost(postId, newPostInfo){
+
+}
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
-
+const db = require('./db');
 app.use(express.json({limit: '50mb'}));
 app.use('/', express.static('./client'));
 require('dotenv').config();
