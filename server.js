61451ad58c4782eef96916abddf90279594c3e2d
Louis <louismartin@umass.edu>
2020-11-20 10:19:42 -0500

    Trying again to fix heroku crash for module not found.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
-const db = require('db.js');
+const db = require('./db.js');
 app.use(express.json({limit: '50mb'}));
 app.use('/', express.static('./client'));
 require('dotenv').config();
